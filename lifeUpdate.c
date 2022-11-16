aadca2814c9f4e81544068fb8727397286a08625
Dan Mahara <dan.mahara.0909@gmail.com>
2022-11-16 18:48:20 +0545

    life Updade

---
+++ lifeUpdate.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+void  choice(){
+char unhappyLife;
+if(unhappyLife==1){
+    printf("sucide");
+}
+
+else if(unhappyLife!=1)
+return -1;
+}
+
+int main(){
+
+
+     choice () ;
+}
