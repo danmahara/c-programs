3a740a8b59eb86caf7461c95865f748cce384157
Dan Mahara <dan.mahara.0909@gmail.com>
2022-11-17 14:42:42 +0545

    update

--- lifeUpdate.c
+++ lifeUpdate.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<conio.h>
 int  main(){
-    
+
 int a;
 printf("Enter your life update:\n");
 printf("If life is happy press 1 \n If not happy press 0:\n");
@@ -10,12 +10,12 @@ scanf("%d",&a);
 if(a==1){
   
 printf("Enjoy:\n");
-//   return -1;
+  return -1;
 }   
 else 
 {
       printf("sucide");
-    // return -1;
+    return -1;
     }
 
     return 0;
