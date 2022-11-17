016934aa3d46c4bf165a537f1f1ef3fb1a07632a
Dan Mahara <dan.mahara.0909@gmail.com>
2022-11-17 08:56:22 +0545

    lifeUpdater

--- lifeUpdate.c
+++ lifeUpdate.c
@@ -1,25 +1,22 @@
 #include<stdio.h>
-
-void  choice(){
-char unhappyLife;
+#include<conio.h>
+int  main(){
+    
 int a;
 printf("Enter your life update:\n");
-printf("If life is happy press 1 if not happy press 0:\n");
+printf("If life is happy press 1 \n If not happy press 0:\n");
 scanf("%d",&a);
-}
 
-if(a==0&&a!=1){
-    printf("sucide");
-
+if(a==1){
+  
+printf("Enjoy:\n");
+//   return -1;
 }   
 else 
+{
+      printf("sucide");
+    // return -1;
+    }
 
     return 0;
-
-
-int main(){
-
-
-
-     choice () ;
 }
