a43a1b3233e398d0143d0891f578d3f2ffdf6390
Dan Mahara <dan.mahara.0909@gmail.com>
2022-12-23 17:18:14 +0545

    update

---
+++ classPractice/allPatterns.c
@@ -0,0 +1,153 @@
+/* Write a program to print this pattern
+        *
+        **
+        ***
+        ****
+        *****
+ */
+// #include <stdio.h>
+// int main()
+// {
+//     for (int i = 1; i <= 5; i++)
+//     {
+//         for (int j = 1; j <= i; j++)
+
+//         {
+//             printf("*");
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
+
+
+
+
+
+/* Write a program to print this pattern
+       *****
+       ****
+       ***
+       **
+       *
+ */
+// #include <stdio.h>
+// int main()
+// {
+//     for (int i = 5; i >= 1; i--)
+//     {
+//         for (int j = 1; j <= i; j++)
+
+//         {
+//             printf("*");
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
+
+
+
+
+
+
+/* Write a program to print this pattern
+       1
+       22
+       333
+       4444
+       55555
+ */
+// #include <stdio.h>
+// int main()
+// {
+//     for (int i = 1; i <= 5; i++)
+//     {
+//         for (int j = 1; j <= i; j++)
+
+//         {
+//             printf("%d",i);
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
+
+
+
+
+
+
+/* Write a program to print this pattern
+       1
+       12
+       123
+       1234
+       12345
+ */
+// #include <stdio.h>
+// int main()
+// {
+//     for (int i = 1; i <= 5; i++)
+//     {
+//         for (int j = 1; j <= i; j++)
+
+//         {
+//             printf("%d",j);
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
+
+
+
+
+
+/* Write a program to print this pattern
+   12345
+   1234
+   123
+   12
+   1
+ */
+// #include <stdio.h>
+// int main()
+// {
+//     for (int i = 5; i >= 1; i--)
+//     {
+//         for (int j = 1; j <= i; j++)
+
+//         {
+//             printf("%d",j);
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
+
+
+
+
+
+/* Write a program to print this pattern
+  55555
+  4444
+  333
+  22
+  1
+ */
+// #include <stdio.h>
+// int main()
+// {
+// int i,j;
+// for ( i = 5; i >=1; i--)
+// {
+//       for ( j = 1; j<=i ; j++)
+//    {
+//     printf("%d",i);
+//    }
+//    printf("\n");
+// }
+//     return 0;
+// }
