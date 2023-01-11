f62bf808e03d026be5c7549d94d5e81dcb91ce90
Dan Mahara <dan.mahara.0909@gmail.com>
2023-01-11 18:26:07 +0545

    formatted

--- forPractical/reverseString.c
+++ forPractical/reverseString.c
@@ -4,18 +4,18 @@ int main()
   char str[1000], rev[1000];
   int i, j, count = 0;
 
-printf("\n\nProgram to reserve a string\n\n");
+  printf("\n\nProgram to reserve a string\n\n");
   printf("\nEnter any String:\t");
   scanf("%s", str);
   printf("\nString Before Reverse: %s", str);
-  //finding the length of the string
+  // finding the length of the string
   while (str[count] != '\0')
   {
     count++;
   }
   j = count - 1;
 
-  //reversing the string by swapping
+  // reversing the string by swapping
   for (i = 0; i < count; i++)
   {
     rev[i] = str[j];
